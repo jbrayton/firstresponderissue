4f908850426d9b610761f21a655a34540624b476
John Brayton <jbrayton@goldenhillsoftware.com>
2018-05-21 09:29:08 -0400

    first commit

--- firstresponderissue/ViewController.swift
+++ firstresponderissue/ViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    var webView: WKWebView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Create a simple webView, add it to the view, and make it first responder.
+        
+        let webView = WKWebView(frame: self.view.bounds)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.load(URLRequest(url: URL(string: "https://www.goldenhillsoftware.com/unread/")!))
+
+        self.view.addSubview(webView)
+        self.view.addConstraint(webView.leftAnchor.constraint(equalTo: self.view.leftAnchor))
+        self.view.addConstraint(webView.rightAnchor.constraint(equalTo: self.view.rightAnchor))
+        self.view.addConstraint(webView.topAnchor.constraint(equalTo: self.view.topAnchor))
+        self.view.addConstraint(webView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor))
+
+        webView.becomeFirstResponder()
+        self.webView = webView
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
+
 
